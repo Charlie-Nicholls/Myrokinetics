fda88395019814ce668c124b25e136a5927cbe2e
Charlie-Nicholls <108279663+Charlie-Nicholls@users.noreply.github.com>
2023-01-03 13:15:49 +0000

    Update __init__.py

--- plotting/__init__.py
+++ plotting/__init__.py
@@ -1,5 +1,5 @@
 from .scan_plotter import plot_scan
-from .eigen_plotter import plot_eigen
+from .eigen_plotter import plot_eigen, plot_single
 from .ideal_plotter import plot_ideal
 from .epar_plotter import plot_epar
 
@@ -8,5 +8,6 @@ Plotters["Scan"] = plot_scan
 Plotters["Eigen"] = plot_eigen
 Plotters["Ideal"] = plot_ideal
 Plotters["Epar"] = plot_epar
+Plotters["Eigen_Single"] = plot_single
 
 __all__ = ["Plotters"]
