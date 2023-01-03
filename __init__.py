6e4c2065abfdbc5bf9963838891cb7ff09f72887
Charlie-Nicholls <108279663+Charlie-Nicholls@users.noreply.github.com>
2023-01-03 13:56:34 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from .myro_reader import myro
 from .myro_scanner import myro_scan
 from .myro_single import myro_single
+from .peqdsk_reader import peqdsk
+from .peqdsk_interpolate import peqdsk_interpolator
 
-__all__ = ["myro", "myro_scan", "myro_single"]
+__all__ = ["myro", "myro_scan", "myro_single","peqdsk","peqdsk_interpolator"]
