354cd041957abe4036ae7929be19cd5e598da2cb
Charlie Nicholls <108279663+Charlie-Nicholls@users.noreply.github.com>
2023-05-25 16:44:31 +0100

    Update equillibrium.py

--- equillibrium.py
+++ equillibrium.py
@@ -274,7 +274,7 @@ class equillibrium(object):
 			nml['gs2_diagnostics_knobs']['write_final_epar'] = False
 		
 		if 'ntheta_geometry' not in nml['theta_grid_eik_knobs'].keys():
-			nml['theta_grid_eik_knobs']['ntheta_geometry'] = 2048
+			nml['theta_grid_eik_knobs']['ntheta_geometry'] = 4096
 
 		if self.inputs['Ideal']:
 			if self.inputs['beta_div'] is None:
