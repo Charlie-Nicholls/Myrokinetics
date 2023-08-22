a068e7bdbeb08307e4e18ef142a416a908999a1b
C Nicholls <cn762@pinch.its.york.ac.uk>
2023-08-22 13:07:44 +0100

    removed set plot from init

--- plotting/__init__.py
+++ plotting/__init__.py
@@ -2,7 +2,6 @@ from .scan_plotter import plot_scan
 from .diagnostics_plotter import plot_diag
 from .ideal_plotter import plot_ideal
 from .epar_plotter import plot_epar
-from .set_plotter import plot_set
 from .theta_plotter import plot_theta, plot_theta_scan, plot_theta_set
 from .ql_plotter import plot_ql
 from .slice_plotter import plot_slice
@@ -12,7 +11,6 @@ Plotters["Scan"] = plot_scan
 Plotters["Diag"] = plot_diag
 Plotters["Ideal"] = plot_ideal
 Plotters["Epar"] = plot_epar
-Plotters["Set"] = plot_set
 Plotters["Theta"] = plot_theta_scan
 Plotters["Theta_Single"] = plot_theta
 Plotters["Theta_Set"] = plot_theta_set
