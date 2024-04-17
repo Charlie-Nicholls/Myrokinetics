0b25b879a15651694de982e53ad53a564eeb0b09
C Nicholls <cn762@balloon.its.york.ac.uk>
2024-04-17 11:39:56 +0100

    bug fix

--- box.py
+++ box.py
@@ -203,8 +203,8 @@ def convert_cgyro_to_myro(filename, input_file, directory = "./", QuickSave = Fa
 	group_data[group_key]['t'] = array(run_data['time'].data).tolist()
 	group_data[group_key]['theta'] = array(run_data['theta']).tolist()
 	hflux = npsum(array(run_data['heat']),axis=0)
-	hflux = npsum(array(run_data['heat']),axis=0)
-	hflux = npsum(array(run_data['heat']),axis=0)
+	hflux = npsum(array(hflux),axis=0)
+	hflux = npsum(array(hflux),axis=0)
 	group_data[group_key]['heat_flux_tot'] = hflux.tolist()
 	phi2_by_ky = []
 	phi2_by_kx = []
