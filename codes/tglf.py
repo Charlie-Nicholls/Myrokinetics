94b592aea19df13a6c3c813b83601c973b913fc1
C Nicholls <cn762@york.ac.uk>
2024-10-29 15:35:58 +0000

    bug fixes

--- codes/tglf.py
+++ codes/tglf.py
@@ -81,7 +81,7 @@ def start_run(run, run_attempt = 1):
 	else:
 		print(f"ERROR: {{run}} took too many attempts to start, skipping")
 
-Parallel(n_jobs={self.inputs['sbatch']['ntasks-per-node']})(delayed(start_run)(run) for run in input_files)""")
+Parallel(n_jobs={scanner.inputs['sbatch']['ntasks-per-node']})(delayed(start_run)(run) for run in input_files)""")
 		pyth.close()
 		return
 		
