5de9cfe7ad0b434146b2b6e978de969fbd95ace3
C Nicholls <cn762@york.ac.uk>
2024-10-31 16:21:26 +0000

    bug fix

--- codes/cgyro.py
+++ codes/cgyro.py
@@ -103,7 +103,7 @@ Parallel(n_jobs={scanner.inputs['sbatch']['nodes']})(delayed(start_run)(run) for
 	def get_non_linear_archer2(self, scanner):
 		infi = list(scanner._input_files)[0]
 		ntasks = scanner.inputs['sbatch']['nodes']*128//scanner.inputs['sbatch']['cpus-per-task']
-		run_code = '''srun --nodes={scanner.inputs['sbatch']['nodes']} --ntasks={ntasks} --cpus-per-task={scanner.inputs['sbatch']['cpus-per-task']} $GACODE_ROOT/cgyro/bin/cgyro -e \"{infi}\" -n {ntasks} -nomp 1 -numa 8 -mpinuma 16 -p \"{infi}\"
+		run_code = f'''srun --nodes={scanner.inputs['sbatch']['nodes']} --ntasks={ntasks} --cpus-per-task={scanner.inputs['sbatch']['cpus-per-task']} $GACODE_ROOT/cgyro/bin/cgyro -e \"{infi}\" -n {ntasks} -nomp 1 -numa 8 -mpinuma 16 -p \"{infi}\"
 if test -f \"{infi[:-3]}.out.nc\"; then
 touch \"{infi[:-3]}.fin\"
 fi'''
--- codes/gs2.py
+++ codes/gs2.py
@@ -234,7 +234,7 @@ Parallel(n_jobs={scanner.inputs['sbatch']['nodes']})(delayed(start_run)(run) for
 	
 	def get_non_linear_archer2(self, scanner):
 		ntasks = scanner.inputs['sbatch']['nodes']*128//scanner.inputs['sbatch']['cpus-per-task']
-		run_code = '''srun --nodes={scanner.inputs['sbatch']['nodes']} --ntasks={ntasks} --cpus-per-task={scanner.inputs['sbatch']['cpus-per-task']} gs2 \"{list(scanner._input_files)[0]}\"
+		run_code = f'''srun --nodes={scanner.inputs['sbatch']['nodes']} --ntasks={ntasks} --cpus-per-task={scanner.inputs['sbatch']['cpus-per-task']} gs2 \"{list(scanner._input_files)[0]}\"
 if test -f \"{list(scanner._input_files)[0][:-3]}.out.nc\"; then
 touch \"{list(scanner._input_files)[0][:-3]}.fin\"
 fi'''
