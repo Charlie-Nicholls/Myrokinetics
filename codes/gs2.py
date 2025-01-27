54aab27befc664df438efba6ce81d4320b0e5296
C Nicholls <cn762@york.ac.uk>
2025-01-27 12:05:56 +0000

    bug fix

--- codes/gs2.py
+++ codes/gs2.py
@@ -1,5 +1,5 @@
 from ..code import code
-from ..ncdf2dict import readnc
+from .ncdf2dict import ncdf2dict as readnc
 from copy import deepcopy
 from numpy import real, imag, array, loadtxt, transpose, nan, trapz
 import os
