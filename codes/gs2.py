7e00f53267e66aecb4ba86aff7755e494fa18e6a
C Nicholls <cn762@york.ac.uk>
2026-01-09 16:30:56 +0000

    bug fix

--- codes/gs2.py
+++ codes/gs2.py
@@ -72,7 +72,7 @@ class gs2(code):
 				nml['knobs']['beta'] = nml['parameters']['beta']
 			del(nml['parameters'])
 		if 'zeff' in nml['knobs'].keys():
-			nml['species_knobs']['zeff'] = nml['species_knobs']['zeff']
+			nml['species_knobs']['zeff'] = nml['knobs']['zeff']
 			del(nml['species_knobs']['zeff'])
 		if 'iflux' in nml['theta_grid_eik_knobs'].keys():
 			del(nml['theta_grid_eik_knobs']['iflux'])
